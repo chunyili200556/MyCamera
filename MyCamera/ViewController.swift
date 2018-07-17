3c10de2979130b0440fb92917c6caef5db1d6e63
chunyili <chunyili200556@gmail>
2018-07-17 10:41:40 +0800

    MyCamera

--- MyCamera/ViewController.swift
+++ MyCamera/ViewController.swift
@@ -8,18 +8,45 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController{
+    
+    @IBOutlet weak var imageView: UIImageView!
+    private let picker = UIImagePickerController()
+    // if you want to set cropped image ratio 16 : 9 , put 16/9 !
+    private let cropper = UIImageCropper(cropRatio: 16/9)
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        cropper.delegate = self // Important !!!!!
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func cameraTapped(_ sender: UIButton) {
+        cropper.picker = picker
+        cropper.cropButtonText = "Crop"
+        cropper.cancelButtonText = "Retake"
+        
+        let controller = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
+        let takePicAction = UIAlertAction(title: "拍照", style: .default) { (_) in
+            self.picker.sourceType = .camera
+            self.present(self.picker, animated: true, completion: nil)
+        }
+        let pickPicAction = UIAlertAction(title: "從相簿選擇照片", style: .default) { (_) in
+            self.picker.sourceType = .photoLibrary
+            self.present(self.picker, animated: true, completion: nil)
+        }
+        let cancelAction = UIAlertAction(title: "取消", style: .cancel, handler: nil)
+        controller.addAction(takePicAction)
+        controller.addAction(pickPicAction)
+        controller.addAction(cancelAction)
+        self.present(controller, animated: true, completion: nil)
     }
+    
+}
 
-
+extension ViewController : UIImageCropperProtocol{
+    func didCropImage(originalImage: UIImage?, croppedImage: UIImage?) {
+        imageView.image = croppedImage
+    }
 }
 
